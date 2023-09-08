cc4b376542a680c98701371be555cbb85b9342d5
Samuel Anjel <moldrek@gmail.com>
2023-09-07 23:21:19 -0300

    fix: added safe controller

--- api/v1/controllers/ProviderController.php
+++ api/v1/controllers/ProviderController.php
@@ -7,7 +7,7 @@ namespace app\api\v1\controllers;
 use app\models\Provider;
 use yii\rest\ActiveController;
 
-class ProviderController extends ActiveController
+class ProviderController extends SafeController
 {
     public $modelClass = Provider::class;
 }
