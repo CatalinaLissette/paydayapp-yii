c0282801fe5b7cfc594cbf83f7156bbd462fa3e4
jairo <30694491+rojaixz@users.noreply.github.com>
2023-12-11 23:00:24 -0300

    fix cors

--- api/v1/controllers/OfferController.php
+++ api/v1/controllers/OfferController.php
@@ -7,9 +7,10 @@ namespace app\api\v1\controllers;
 use app\models\Offer;
 use app\services\OfferService;
 use Yii;
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
-class OfferController extends ActiveController//SafeController
+class OfferController extends ActiveController
 {
     public $modelClass = Offer::class;
     private OfferService $offerService;
@@ -24,6 +25,17 @@ class OfferController extends ActiveController//SafeController
         parent::__construct($id, $module, $config);
         $this->offerService = $offerService;
     }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['cors'] = [
+            'class' => Cors::class,
+        ];
+
+        return $behaviors;
+    }
+
     public function actionCreate()
     {
         $requestData = Yii::$app->request->getBodyParams();
--- api/v1/controllers/ProviderController.php
+++ api/v1/controllers/ProviderController.php
@@ -5,9 +5,20 @@ namespace app\api\v1\controllers;
 
 
 use app\models\Provider;
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
-class ProviderController extends SafeController
+class ProviderController extends ActiveController
 {
     public $modelClass = Provider::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['cors'] = [
+            'class' => Cors::class,
+        ];
+
+        return $behaviors;
+    }
 }
