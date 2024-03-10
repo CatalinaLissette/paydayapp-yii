f9b75cc95e3a8d24006411ab793ad7929428d8c3
Samuel Anjel <moldrek@gmail.com>
2024-03-10 17:21:32 -0300

    fix: refactor commerce get providers

--- config/params.php
+++ config/params.php
@@ -8,6 +8,6 @@ return [
         'issuer' => 'https://api.example.com',  //name of your project (for information only)
         'audience' => 'https://frontend.example.com',  //description of the audience, eg. the website using the authentication (for info only)
         'id' => '',  //a unique identifier for the JWT, typically a random string
-        'expire' => 300,
+        'expire' => 86400,
     ],
 ];
